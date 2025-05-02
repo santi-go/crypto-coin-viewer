7f83059a85650ed42752591c77f7d746ccd2495b
Santiago Gonzalez Erazo <santiago.gonzalez.erazo@gmail.com>
2025-05-02 12:19:23 +0200

    fix build compile type error

--- src/app/coin/[id]/page.tsx
+++ src/app/coin/[id]/page.tsx
@@ -2,10 +2,10 @@ import { fetchCoinById } from '@/app/services/coinService';
 import { CoinDetails } from '../../interfaces/coinViewer.interface';
 import CoinCard from '@/app/components/coinCard';
 
-export default async function CoinPage({ params }: { params: { id: string } }) {
-  const { id } = params;
+export default async function CoinPage(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
   const coin: CoinDetails = await fetchCoinById(id);
-  console.log('coin',coin);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
